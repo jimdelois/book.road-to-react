6c97335927a2bba83c0f352e1df507e3df079964
Jim DeLois <jimdelois@users.noreply.github.com>
2021-03-22 18:36:34 -0400

    Initial App through Page 72

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
 function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan White',
+      num_comments: 3,
+      points: 4,
+      objectId: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://reduxjs.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectId: 1,
+    }
+  ];
+
+  const useSemiPersistedState = (key, initialValue) => {
+    const [value, setValue] = React.useState(
+      localStorage.getItem(key) || initialValue
+    );
+
+    React.useEffect(() => {
+      localStorage.setItem(key, value);
+    }, [value]);
+
+    return [value, setValue];
+  };
+
+  const [searchTerm, setSearchTerm] = useSemiPersistedState('search', '');
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value)
+  };
+
+  const searchedStories = stories.filter(
+    story => story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search searchTerm={searchTerm} onSearch={handleSearch}/>
+
+      <hr />
+
+      <List list={searchedStories}/>
+    </div>
+  );
+}
+
+
+const Search = ({searchTerm, onSearch}) => {
+  return (
+    <>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" value={searchTerm} onChange={onSearch}/>
+
+      <p>Searching for <strong>{searchTerm}</strong></p>
+    </>
+  );
+}
+
+const List = ({list}) => {
+  return list.map(({objectId, ...item}) => <Item key={objectId} {...item} />);
+}
+
+const Item = ({url, title, author, num_comments, points}) => {
+  return (
+    <div>
+      <span><a href={url}>{title}</a></span>
+      <span>{author}</span>
+      <span>{num_comments}</span>
+      <span>{points}</span>
     </div>
   );
 }
